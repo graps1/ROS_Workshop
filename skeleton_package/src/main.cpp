8e612544f94ad501603d0c86549f4528b547bb8a
OliEfr <ollihaus@t-online.de>
2018-05-03 14:28:24 +0200

    oli uptade

--- skeleton_package/src/main.cpp
+++ skeleton_package/src/main.cpp
@@ -10,8 +10,8 @@ int main (int argc, char **argv) {
     ros::init(argc, argv, "skeleton_node");
     ros::NodeHandle nh;
 
-    ros::Publisher pub = nh.advertise<data::type>("/topic_name", 1);
-    ros::Subscriber sub = handle.subscribe("/topic_Name", 100, Callback);
+    // ros::Publisher pub = nh.advertise<data::type>("/topic_name", 1);
+    // ros::Subscriber sub = handle.subscribe("/topic_Name", 100, Callback);
     
 
     while (ros::ok()) {
