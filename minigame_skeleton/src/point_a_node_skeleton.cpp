b067eb2e7b0b4e1056430f14547966b29e6f3707
OliEfr <ollihaus@t-online.de>
2018-05-03 16:30:14 +0200

    finalized without can

--- minigame_skeleton/src/point_a_node_skeleton.cpp
+++ minigame_skeleton/src/point_a_node_skeleton.cpp
@@ -1,51 +1,48 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PointStamped.h>
-#include <dynamic_reconfigure/server.h>
-#include <minigame_skeleton/point_a_nodeConfig.h>
+
 
 ros::Publisher pub_position;
 float current_pos_x;
 float current_pos_y;
-
-void callback(minigame_skeleton::point_a_nodeConfig &config, uint32_t level) {
-  ROS_INFO("Reconfigure Request: x: %f y: %f", 
-            config.current_pos_x, config.current_pos_y);
-
-  current_pos_x = config.current_pos_x;
-  current_pos_y = config.current_pos_y;  
-  
-}
+float start_x, start_y;
 
 /** Main node entry point. */
 int main(int argc, char **argv)
 {
+    
+    //initialisiere Node
     ros::init(argc, argv, "point_a_node");
     ros::NodeHandle nh;
 
+    //create publisher for PointA
     pub_position = nh.advertise<geometry_msgs::PointStamped>("/positionA", 1);
-    dynamic_reconfigure::Server<minigame_skeleton::point_a_nodeConfig> server;
-    dynamic_reconfigure::Server<minigame_skeleton::point_a_nodeConfig>::CallbackType f;
 
-    f = boost::bind(&callback, _1, _2);
-    server.setCallback(f);
+    
+    
+    //das sollen die veränderbaren startpunkte werden
+    start_x = 0;
+    start_y = 0;
+  
 
-    float start_x, start_y;
-    nh.param("start_x", start_x);
-    nh.param("start_y", start_y);
-    current_pos_x = start_x;
-    current_pos_y = start_y;    
+    //das sollen die dynamisch rekonfigurierbaren Punkte werden
+    current_pos_y = 0;
+    current_pos_x = 0;  
 
+    //PunktA erstellen
     geometry_msgs::PointStamped pointStampedA;
-    pointStampedA.header.frame_id = "/map";
-    pointStampedA.point.x = start_x;
-    pointStampedA.point.y = start_y;    
+    pointStampedA.header.frame_id = "/map"; 
 
-  	ros::Rate rate(100); // 10 hz    
+  	ros::Rate rate(10); // 10 hz    
+   
     while (ros::ok()) {
       pointStampedA.header.stamp = ros::Time::now();
+      
+      //dynamisch rekonfigurierbare werte schreiben
       pointStampedA.point.x = current_pos_x;
       pointStampedA.point.y = current_pos_y;    
       
+      //publishen
       pub_position.publish(pointStampedA);
       rate.sleep();
       ros::spinOnce();
---
+++ minigame_skeleton/src/point_b_node_.cpp
@@ -0,0 +1,43 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PointStamped.h>
+
+/* Diese Node lässt einen PunktB zufällig auf der x-y-Ebene bewegen */
+
+
+ros::Publisher pub_position;
+
+/** Main node entry point. */
+int main(int argc, char **argv)
+{
+
+  //node initialisieren
+  ros::init(argc, argv, "point_b_node");
+  ros::NodeHandle nh;
+
+  //publisher definieren 
+  pub_position = nh.advertise<geometry_msgs::PointStamped>("/positionB", 1);
+
+  //PunktB erstellen
+  geometry_msgs::PointStamped pointStampedB;
+  pointStampedB.header.frame_id = "/map";
+
+  //fängt in punkt 0,0 an
+  pointStampedB.point.x = 0;
+  pointStampedB.point.y = 0; 
+
+  ros::Rate rate(2); // 2 hz
+  while (ros::ok()) {
+
+    //set new random position
+    pointStampedB.point.x += ( (rand() % 100) / 50.0 ) - 1.0;
+    pointStampedB.point.y += ( (rand() % 100) / 50.0 ) - 1.0;  
+      
+    //publish point  
+    pointStampedB.header.stamp = ros::Time::now();
+    pub_position.publish(pointStampedB); 
+    rate.sleep(); 
+    ros::spinOnce();
+	  
+  }
+  return 0;
+}
